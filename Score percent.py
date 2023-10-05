1a423f8f4d31b621eef08b363dadebf382b0f4ba
MuthuManikandanP <118056009+MuthuManikandanP@users.noreply.github.com>
2023-10-05 22:14:27 +0530

    Update Score percent.py

--- Score percent.py
+++ Score percent.py
@@ -1,3 +1,5 @@
+#Get a input for score percentage. Only If the percentage is greater than 70, get input for his name, department and location. Then print you are elgibile. If not print you are not eligible.
+
 score=int(input("Enter the Score Percent:"))
 if(score>=70):
     name=input("enter the name:")
